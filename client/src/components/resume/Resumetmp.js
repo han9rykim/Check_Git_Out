0a3fbc57eb41d17c9e17295aa4c6de5d8ee64f7e
dblepart99 <minbmw8205@gmail.com>
2021-11-23 15:36:06 +0900

    modify for tp

--- client/src/components/resume/Resumetmp.js
+++ client/src/components/resume/Resumetmp.js
@@ -1,3 +1,4 @@
+//Real Resume.js
 import React, { useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
@@ -6,22 +7,11 @@ import ResumeProfile from "./ResumeProfile";
 const ReadmeBackGroundBlock = styled.div`
   position: absolute;
   overflow: scroll;
-  width: 90%;
-  height: 90%;
-  margin: auto;
-  top: 60px;
-  background: #ffffff;
-  border: 1px solid #000000;
-  border-radius: 10px;
-`;
-
-const ResumeBackGroundBlock = styled.div`
-  position: absolute;
-  width: 665px;
+  width: 870px;
   height: 830px;
   left: 100px;
   top: 80px;
-  background: rgba(197, 197, 197, 0.65);
+  background: #ffffff;
   border: 1px solid #000000;
   border-radius: 10px;
   margin: 0 auto;
@@ -29,29 +19,122 @@ const ResumeBackGroundBlock = styled.div`
   padding-left: 30px;
   margin-bottom: 32px;
 `;
-const ReadmeUserNameBlock = styled.div`
-  position: absolute;
-  width: 80%;
-  top: 10px;
-  height: 40px;
-  left: 60px;
-  background: #ffffff;
-  border-radius: 10px;
-  font-size: 30px;
-`;
 
 const CommitBlock = styled.div`
   position: absolute;
   width: 665px;
-  height: 830px;
+  height: 410px;
   left: 1085px;
-  top: 80px;
+  top: 500px;
   background: rgba(197, 197, 197, 0.65);
   border-radius: 10px;
 `;
 
-/**
- * const GetBlock = styled.button`
+const CommitLogBlock = styled.div`
+  position: absolute;
+  width: 665px;
+  height: 390px;
+  left: 1085px;
+  top: 80px;
+  margin: 0 auto;
+  background: rgba(197, 197, 197, 0.65);
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
+
+const CommitLogTitle = styled.div`
+  position: absolute;
+  width: 606px;
+  height: 55px;
+  left: 1115px;
+  top: 105px;
+  background: rgba(22, 65, 148, 0.8);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 60px;
+  color: #ffffff;
+`;
+
+const CommitLog = styled.div`
+  position: absolute;
+  width: 606px;
+  height: 240px;
+  left: 1115px;
+  top: 180px;
+  background: rgba(22, 65, 148, 0.8);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 35px;
+  color: #ffffff;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+const TitleBlock = styled.input`
+  position: absolute;
+  width: 605px;
+  height: 50px;
+  left: 1115px;
+  top: 580px;
+  background: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: block;
+  color: #8d8d8d;
+  background: #ffffff;
+`;
+
+const DateBlock = styled.input`
+  position: absolute;
+  width: 605px;
+  height: 55px;
+  left: 1115px;
+  top: 510px;
+  background: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: block;
+  color: #8d8d8d;
+  background: #ffffff;
+`;
+
+const DescriptionBlock = styled.input`
+  position: absolute;
+  width: 605px;
+  height: 200px;
+  left: 1115px;
+  top: 640px;
+  background: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: block;
+  color: #8d8d8d;
+  background: #ffffff;
+`;
+
+const CheckBlock = styled.button`
+  position: absolute;
+  width: 100px;
+  height: 30px;
+  left: 1360px;
+  top: 850px;
+  color: white;
+  background: rgba(22, 65, 148, 0.8);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
+const GetBlock = styled.button`
   position: absolute;
   width: 100px;
   height: 30px;
@@ -112,110 +195,6 @@ const MakePRBlock = styled.button`
   border-radius: 10px;
   color: white;
 `;
-const CommitLogBlock = styled.div`
-  position: absolute;
-  width: 665px;
-  height: 390px;
-  left: 1085px;
-  top: 80px;
-  margin: 0 auto;
-  background: rgba(197, 197, 197, 0.65);
-  border-radius: 10px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-`;
-
-const CommitLogTitle = styled.div`
-  position: absolute;
-  width: 606px;
-  height: 55px;
-  left: 1115px;
-  top: 105px;
-  background: rgba(22, 65, 148, 0.8);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 40px;
-  line-height: 60px;
-  color: #ffffff;
-`;
-
-const CommitLog = styled.div`
-  position: absolute;
-  width: 606px;
-  height: 240px;
-  left: 1115px;
-  top: 180px;
-  background: rgba(22, 65, 148, 0.8);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 35px;
-  color: #ffffff;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-`;
-
- */
-const DateBlock = styled.input`
-  position: absolute;
-  width: 605px;
-  height: 55px;
-  left: 1115px;
-  top: 170px;
-  background: white;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  display: block;
-  color: #8d8d8d;
-  background: #ffffff;
-`;
-const TitleBlock = styled.input`
-  position: absolute;
-  width: 605px;
-  height: 50px;
-  left: 1115px;
-  top: 100px;
-  background: white;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  display: block;
-  color: #8d8d8d;
-  background: #ffffff;
-`;
-
-const DescriptionBlock = styled.input`
-  position: absolute;
-  width: 605px;
-  height: 580px;
-  left: 1115px;
-  top: 250px;
-  background: white;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  display: block;
-  color: #8d8d8d;
-  background: #ffffff;
-`;
-
-const CheckBlock = styled.button`
-  position: absolute;
-  width: 100px;
-  height: 30px;
-  left: 1360px;
-  top: 850px;
-  color: white;
-  background: rgba(22, 65, 148, 0.8);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-`;
 
 function Resume({ history, match }) {
   const token = localStorage.getItem("access_token");
@@ -245,13 +224,9 @@ function Resume({ history, match }) {
     setSendReq(nextsendReq);
   };
 
-  const addCommitLog = (e) => {
+  const addCommitLog = () => {
     // e.preventDefault();
-    // console.log("확이버튼");
-    if (date.length == 0 || title.length == 0 || title.description == 0) {
-      alert("공백이 있습니다!");
-      return;
-    }
+    console.log("확이버튼");
     alert(`[${date}] [${title}] - ${description}`);
     setSendReq({
       date: "",
@@ -338,16 +313,12 @@ function Resume({ history, match }) {
 
   return (
     <div>
-      <ResumeBackGroundBlock>
-        <ReadmeUserNameBlock>{username}</ReadmeUserNameBlock>
-        <ReadmeBackGroundBlock>
-          <ResumeProfile props={username} />
-        </ReadmeBackGroundBlock>
-      </ResumeBackGroundBlock>
-
-      {/* <CommitLogBlock /> */}
-      {/* <CommitLogTitle>Commit Log</CommitLogTitle> */}
-      {/* <CommitLog>{commitContent}</CommitLog> */}
+      <ReadmeBackGroundBlock>
+        <ResumeProfile props={username} />
+      </ReadmeBackGroundBlock>
+      <CommitLogBlock />
+      <CommitLogTitle>Commit Log</CommitLogTitle>
+      <CommitLog>{commitContent}</CommitLog>
 
       <CommitBlock />
       <DateBlock
@@ -356,7 +327,6 @@ function Resume({ history, match }) {
         placeholder="date"
         value={date}
         onChange={onChange}
-        required={true}
       />
 
       <TitleBlock
@@ -365,7 +335,6 @@ function Resume({ history, match }) {
         placeholder="title"
         value={title}
         onChange={onChange}
-        required={true}
       />
 
       <DescriptionBlock
@@ -374,10 +343,8 @@ function Resume({ history, match }) {
         placeholder="description"
         value={description}
         onChange={onChange}
-        required={true}
         // onKeyPress={onKeyPress}
       />
-
       <div>
         <CheckBlock
           onClick={(e) => {
@@ -387,7 +354,7 @@ function Resume({ history, match }) {
         >
           저장
         </CheckBlock>
-        {/*
+
         <GetBlock
           // onClick={(e) => {
           //   e.preventDefault();
@@ -432,7 +399,7 @@ function Resume({ history, match }) {
           }}
         >
           Delete ALL
-        </DeleteAllBlock>*/}
+        </DeleteAllBlock>
       </div>
     </div>
   );
